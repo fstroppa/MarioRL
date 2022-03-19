b86f94b30261fd43da78c7bedad0e2f85738da13
fstroppa <73309095+fstroppa@users.noreply.github.com>
2022-03-19 17:51:30 +0100

    transformed into class

--- MarioQLearning.py
+++ MarioQLearning.py
@@ -23,84 +23,132 @@ q_table = np.random.random(size=(400 * 120, 2))
 episode_rewards = []
 
 
-def get_action():
-    if np.random.random() > epsilon:
-        action = np.argmax(q_table[key])
-    else:
-        action = np.random.randint(0, 1)
-    return action
+class Episode:
+    def __init__(self, env, episode_number):
+        self.env = env
+        self.show = self.show_episode(episode_number)
+        self.env.reset()
+        _, _, self.done, self.info = env.step([restricted_actions_list[0]])
+        self.frame = 0
+        self.episode_reward = 0
+        self.done = False
+        self.last_x = []
+        self.last_key = []
 
-
-def show_episode():
-    if episode % SHOW_EVERY == 0:
-        print(f"on #{episode}, epsilon is {epsilon}")
-        print(f"{SHOW_EVERY} ep mean: {np.mean(episode_rewards[-SHOW_EVERY:])}")
-        show = True
-    else:
-        show = False
-    return show
-
-
-for episode in range(HM_EPISODES):
-    show = show_episode()
-
-    env.reset()
-    ob, rew, done, info = env.step([restricted_actions_list[0]])
-    frame = 0
-    episode_reward = 0
-    done = False
-    win = False
-    last_x = []
-    last_key = []
-
-    while not done or not win:
-        if show:
-            env.render()
-            key = frame
-            action = np.argmax(q_table[key])
-            _, _, done, info = env.step([restricted_actions_list[action]])
-            last_x.append(info['x'])
-            frame += 1
-
-            if last_x[-1] == min(last_x[-10:]) and frame > 10:
-                break
-
+    def show_episode(self, episode_number):
+        if episode_number % SHOW_EVERY == 0:
+            print(f"on #{episode_number}, epsilon is {epsilon}")
+            print(f"{SHOW_EVERY} ep mean: {np.mean(episode_rewards[-SHOW_EVERY:])}")
+            show = True
         else:
-            key = frame
+            show = False
+        return show
 
-            action = get_action()
+    def get_action(self):
+        if np.random.random() > epsilon or self.show:
+            action = np.argmax(q_table[self.frame])
+        else:
+            action = np.random.randint(0, 1)
+        return action
 
-            last_position = info['x']
-            _, _, done, info = env.step([restricted_actions_list[action]])
-            rew = info['x'] - last_position - 0.5
+    def run(self):
+        key = self.frame
+        action = self.get_action()
 
-            last_key.append((key, action))
-            last_x.append(info['x'])
-            if last_x[-1] == min(last_x[-30:]) and frame > 30:
-                rew = -100
-                done = True
-            frame += 1
+        last_position = self.info['x']
+        _, _, self.done, self.info = env.step([restricted_actions_list[action]])
+        if self.show:
+            episode.env.render()
+        rew = self.info['x'] - last_position - 0.5
 
-            new_key = frame
-            max_future_q = np.max(q_table[new_key])
-            current_q = q_table[key][action]
+        self.last_key.append((key, action))
+        self.last_x.append(self.info['x'])
+        if self.last_x[-1] == min(self.last_x[-30:]) and self.frame > 30:
+            rew = -10
+            self.done = True
+            i = 1
+            for tuple_key_action in self.last_key[-30:]:
+                q_table[tuple_key_action[0]][tuple_key_action[1]] -= rew / i
+                i += 1
+        self.frame += 1
 
-            if info['win'] > 0:
-                new_q = 100000
-            else:
-                new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (rew + DISCOUNT * max_future_q)
-            q_table[key][action] = new_q
+        new_key = self.frame
+        max_future_q = np.max(q_table[new_key])
+        current_q = q_table[key][action]
 
-            episode_reward += rew
-            if info['win'] > 0:
-                win = True
+        if info['win'] > 0:
+            new_q = 100000
+            self.done = True
+        else:
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (rew + DISCOUNT * max_future_q)
+        q_table[key][action] = new_q
 
-            if done:
-                i = 1
-                for tuple_key_action in last_key[-30:]:
-                    q_table[tuple_key_action[0]][tuple_key_action[1]] = q_table[tuple_key_action[0]][tuple_key_action[1]] - 10 / i
-                    i += 1
-                break
+        self.episode_reward += rew
 
-    episode_rewards.append(episode_reward)
+
+
+
+for episode_number in range(HM_EPISODES):
+    episode = Episode(env, episode_number)
+    # show = show_episode()
+
+    # env.reset()
+    # ob, rew, done, info = env.step([restricted_actions_list[0]])
+    # frame = 0
+    # episode_reward = 0
+    # done = False
+    # win = False
+    # last_x = []
+    # last_key = []
+
+    while not episode.done:
+        episode.run()
+            # episode.env.render()
+            # key = frame
+            # action = np.argmax(q_table[key])
+            # _, _, done, info = env.step([restricted_actions_list[action]])
+            # last_x.append(info['x'])
+            # frame += 1
+            #
+            # if last_x[-1] == min(last_x[-10:]) and frame > 10:
+            #     break
+
+        # else:
+        #     key = frame
+        #
+        #     action = get_action()
+        #
+        #     last_position = info['x']
+        #     _, _, done, info = env.step([restricted_actions_list[action]])
+        #     rew = info['x'] - last_position - 0.5
+        #
+        #     last_key.append((key, action))
+        #     last_x.append(info['x'])
+        #     if last_x[-1] == min(last_x[-30:]) and frame > 30:
+        #         rew = -100
+        #         done = True
+        #     frame += 1
+        #
+        #     new_key = frame
+        #     max_future_q = np.max(q_table[new_key])
+        #     current_q = q_table[key][action]
+        #
+        #     if info['win'] > 0:
+        #         new_q = 100000
+        #     else:
+        #         new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (rew + DISCOUNT * max_future_q)
+        #     q_table[key][action] = new_q
+        #
+        #     episode_reward += rew
+        #     if info['win'] > 0:
+        #         win = True
+        #
+        #     if done:
+        #         i = 1
+        #         for tuple_key_action in last_key[-30:]:
+        #             q_table[tuple_key_action[0]][tuple_key_action[1]] = q_table[tuple_key_action[0]][tuple_key_action[1]] - 10 / i
+        #             i += 1
+        #         break
+
+    episode_rewards.append(episode.episode_reward)
     epsilon *= EPS_DECAY
