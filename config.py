764738e3682e340995e0397472656ac1480dc1c8
fstroppa <73309095+fstroppa@users.noreply.github.com>
2022-03-19 17:15:19 +0100

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+# Configuration paramaters for the whole setup
+seed = 42
+# restricted_actions_dict = {'B': 1, 'Y': 2, 'A': 256, 'LEFT': 64, 'RIGHT': 128, 'UP': 16, 'DOWN': 32, 'Y+RIGHT': 130}
+restricted_actions_dict = {'Y+RIGHT': 130, 'B+Y+RIGHT':131}
+HM_EPISODES = 250000
+SHOW_EVERY = 10
+
+epsilon = 0.9
+EPS_DECAY = 0.9998
+
+LEARNING_RATE = 0.02
+DISCOUNT = 0.95
