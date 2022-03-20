57a736e57ba0bc79e8742c6f4e00c1efe499f6c0
fstroppa <73309095+fstroppa@users.noreply.github.com>
2022-03-20 16:29:02 +0100

    refactors

--- MarioQLearning.py
+++ MarioQLearning.py
@@ -32,13 +32,13 @@ class Episode:
         self.actions_taken = []
         self.win = False
 
-    def show_episode(self):
+    def _show_episode(self):
         return self.episode_number % SHOW_EVERY == 0
 
     def _frame_decision_is_safe(self):
         return self.frame < self.safe_frame
 
-    def get_action(self):
+    def _get_action(self):
         if self._frame_decision_is_safe():
             return actions_array[self.frame]
         else:
@@ -46,42 +46,45 @@ class Episode:
                 return actions_array[self.frame]
             return np.random.randint(NUMBER_OF_POSSIBLE_ACTIONS)
 
-    def run(self):
-        action = self.get_action()
+    def run_one_step(self):
+        action = self._get_action()
         self._take_actions(action)
         self.actions_taken.append(action)
         self.frame += 1
         self.last_x_positions.append(self.info['x'])
 
+        if self._show_episode():
+            self.env.render()
+
         if self.info['win'] > 0:
             self.done = True
             self.win = True
+            self._save_winning_results()
 
-        if self.show_episode():
-            self.env.render()
-
-        if self.dead_or_stuck():
-            self._save_results_of_the_run()
+        if self._dead_or_stuck():
+            self.done = True
+            self._save_losing_results()
 
     def _take_actions(self, action):
         for _ in range(NUMBER_OF_FRAMES_PER_ACTION):
             _, _, self.done, self.info = self.env.step(
                 [restricted_actions_list[action]])
 
-    def _save_results_of_the_run(self):
-        self.done = True
-        if self.win:
-            for idx, action_take in enumerate(self.actions_taken):
-                actions_array[idx] = action_take
-        else:
-            for idx, action_take in enumerate(self.actions_taken):
-                if idx > self.safe_frame - SAFE_FRAMES:
-                    actions_array[idx] = action_take
-            if len(self.actions_taken) - SAFE_FRAMES > self.safe_frame:
-                self.safe_frame = len(self.actions_taken) - SAFE_FRAMES
 
-    def dead_or_stuck(self):
-        return self.last_x_positions[-1] == min(self.last_x_positions[-DEAD_OR_STUCK:]) and self.frame > 30
+    def _save_losing_results(self):
+        for idx, action_take in enumerate(self.actions_taken):
+            if idx > self.safe_frame - SAFE_FRAMES:
+                actions_array[idx] = action_take
+        if len(self.actions_taken) - SAFE_FRAMES > self.safe_frame:
+            self.safe_frame = len(self.actions_taken) - SAFE_FRAMES
+
+    def _save_winning_results(self):
+        for idx, action_take in enumerate(self.actions_taken):
+            actions_array[idx] = action_take
+
+    def _dead_or_stuck(self):
+        return self.last_x_positions[-1] == min(self.last_x_positions[-DEAD_OR_STUCK:]) \
+               and self.frame > 30
 
 
 if __name__ == '__main__':
@@ -97,7 +100,7 @@ if __name__ == '__main__':
         episode = Episode(env, episode_number, safe_frame)
 
         while not episode.done:
-            episode.run()
+            episode.run_one_step()
         safe_frame = episode.safe_frame
         print(f'Run number {episode_number}: {safe_frame * NUMBER_OF_FRAMES_PER_ACTION} safe frames advanced.')
         if episode_number % 20 == 0:
